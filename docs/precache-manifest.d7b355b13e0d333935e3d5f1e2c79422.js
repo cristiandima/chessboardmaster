857c861663e673afbc022547f25ce6308e866664
Cristian Dima <cristian@cristiandima.com>
2020-09-18 23:38:08 +0300

    publish game gh page from docs folder

---
+++ docs/precache-manifest.d7b355b13e0d333935e3d5f1e2c79422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39f5bdbbb0a6efcd9c0b08fd618ae3d",
+    "url": "/chessboardmaster/index.html"
+  },
+  {
+    "revision": "44c6c15ea8a45c0baf6f",
+    "url": "/chessboardmaster/static/css/main.70b3a6b0.chunk.css"
+  },
+  {
+    "revision": "a85217fbfafb45ae5ff2",
+    "url": "/chessboardmaster/static/js/2.4efc8be7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chessboardmaster/static/js/2.4efc8be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c6c15ea8a45c0baf6f",
+    "url": "/chessboardmaster/static/js/main.b26194ce.chunk.js"
+  },
+  {
+    "revision": "d6ad3da114b5dc6a2490",
+    "url": "/chessboardmaster/static/js/runtime-main.fbe78e2e.js"
+  }
+]);
